ae69f3c574cf4c1ce5b1d3dc937b79f288e64d5b
etg6550 <etg9307@naver.com>
2019-01-16 16:39:41 +0900

    #2741#2742

---
+++ OneStage/Q2741.java
@@ -0,0 +1,13 @@
+package BaekJoon.OneStage;
+
+import java.util.Scanner;
+
+public class Q2741 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int K = sc.nextInt();
+        for(int i=1; i<=K; i++) {
+            System.out.println(i);
+        }
+    }
+}
---
+++ OneStage/Q2742.java
@@ -0,0 +1,14 @@
+package BaekJoon.OneStage;
+
+import java.util.Scanner;
+
+public class Q2742 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int K = sc.nextInt();
+
+        for(int i=K; i>0; i--) {
+            System.out.println(i);
+        }
+    }
+}
