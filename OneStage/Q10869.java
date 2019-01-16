9e480a974bc84df1bf6339eadf5690847eaae844
etg6550 <etg9307@naver.com>
2019-01-16 16:02:47 +0900

    #10430%20#10869

--- OneStage/Q10869.java
+++ OneStage/Q10869.java
@@ -1,4 +1,20 @@
 package BaekJoon.OneStage;
 
+import java.util.Scanner;
+
 public class Q10869 {
+    public static void main(String[] args)  {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(a + b);
+//        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+
+    }
 }
