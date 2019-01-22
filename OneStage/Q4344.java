cf1c0ac3d67804bee43c19db69dccff8b527a477
etg6550 <etg9307@naver.com>
2019-01-22 14:04:52 +0900

    SpringSecurity,Oauth2 set up, property binding

---
+++ OneStage/Q4344.java
@@ -0,0 +1,18 @@
+//https://www.acmicpc.net/problem/4344 평균은넘겠지
+package BaekJoon.OneStage;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q4344 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int K = Integer.parseInt(br.readLine());
+
+        for(int i=0; i<K; i++) {
+
+        }
+    }
+}
