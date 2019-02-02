7f2c499d4b6dda9fb90ae740b4dd846c88989db1
etg6550 <etg9307@naver.com>
2019-02-02 18:21:50 +0900

    #1065

--- OneStage/Q1065.java
+++ OneStage/Q1065.java
@@ -1,4 +1,15 @@
+// https:www.acmicpc.net/problem/1065 한수
+
 package BaekJoon.OneStage;
 
+import java.util.Scanner;
+
 public class Q1065 {
+    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+        double d = 1.342424242;
+
+        System.out.println(d);
+        System.out.println(String.format("%.3f", d));
+    }
 }
