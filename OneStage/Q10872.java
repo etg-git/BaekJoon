65c880b1d368db971505160e5b409c5b4ba55135
etg6550 <etg9307@naver.com>
2019-02-09 10:14:14 +0900

    #10872

--- OneStage/Q10872.java
+++ OneStage/Q10872.java
@@ -1,4 +1,24 @@
+//https://www.acmicpc.net/problem/10872 팩토리얼
 package BaekJoon.OneStage;
 
+import java.util.Scanner;
+
 public class Q10872 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int K = sc.nextInt();
+
+        System.out.println(fact(K));
+    }
+
+    static int fact(int K) {
+        int num = 1;
+        for (int i = K; i >= 1; i--) {
+            num = num * i;
+        }
+        return num;
+
+    }
 }
+
