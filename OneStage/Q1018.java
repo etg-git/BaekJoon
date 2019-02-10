4ebeb62113b3aea7725956ed04bee4dc16c9072e
etg6550 <etg9307@naver.com>
2019-02-10 16:35:37 +0900

    #1018

--- OneStage/Q1018.java
+++ OneStage/Q1018.java
@@ -1,4 +1,21 @@
 package BaekJoon.OneStage;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Q1018 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String one = br.readLine();
+
+        int a = one.charAt(0) - '0';
+        int b = one.charAt(2) - '0';
+
+        for(int i=0; i<a; i++) {
+            String chess = br.readLine();
+    }
+
+    }
 }
