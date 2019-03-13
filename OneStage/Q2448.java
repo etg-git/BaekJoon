9cfbfe63f7c6d9726fc92dea0fd83d252be77354
etg6550 <etg9307@naver.com>
2019-03-13 09:58:17 +0900

    STAR

--- OneStage/Q2448.java
+++ OneStage/Q2448.java
@@ -1,4 +1,21 @@
 package BaekJoon.OneStage;
 
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Q2448 {
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static char[] star;
+
+    public static void main(String[] args) throws IOException {
+        int K = Integer.parseInt(br.readLine());
+        star = new char[K];
+
+        char c = ' ';
+        for(int i=0; i<K; i++) {
+
+        }
+    }
 }
