55cbacb88665f2d3ad3035e7bee6160e3aab9ea3
etg6550 <etg9307@naver.com>
2019-03-13 09:58:51 +0900

    #2740

--- OneStage/Q2740.java
+++ OneStage/Q2740.java
@@ -1,4 +1,23 @@
 package BaekJoon.OneStage;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Q2740 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String A = br.readLine();
+        String A = br.readLine();
+        String A = br.readLine();
+        String A = br.readLine();
+        String A = br.readLine();
+
+
+        for(int i=0; i<K; i++) {
+
+        }
+    }
 }
