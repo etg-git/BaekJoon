5743d5cc88fceedcbf77e1c149cdd27898cd5a1e
etg6550 <etg9307@naver.com>
2019-03-13 09:57:30 +0900

    STAR

--- OneStage/Q2447.java
+++ OneStage/Q2447.java
@@ -1,41 +1,39 @@
 //https://www.acmicpc.net/problem/2447 별찍기 - 10
 package BaekJoon.OneStage;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Q2447 {
+
+    private static final Scanner sc = new Scanner(System.in);
+    private static char[][] star;
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        int K = sc.nextInt();
+        star = new char[K][K];
 
-        int a = sc.nextInt();
+        for(int i=0; i<K; i++) {
+            Arrays.fill(star[i], ' ');
+        }
 
-        String star = "";
-        String[] K = new String[a+1];
-        for(int i =1; i<=a; i++) {
-            if(i % 3 == 0) {
-                for(int j=1; j<=a; j++) {
-                    star = star + "*";
+        Map(0,0,K);
+        for(int i=0; i<K; i++) {
+            System.out.println(star[i]);
+        }
+    }
+    public static void Map(int a, int b, int n) {
+        if(n==1) {
+            star[a][b] = '*';
+            return;
+        }
+        for(int i=0; i<3; i++) {
+            for(int j=0; j<3; j++) {
+                if(i==1 && j==1) {
+                    continue;
                 }
+                Map(a+(n/3)*i, b+(n/3)*j, n/3);
             }
-            else if(i % 3 == 2) {
-                for (int j=1; j<=a; j++) {
-                    if(j%3 == 2) {
-                        star = star + " ";
-                    }
-                    else {
-                        star = star + "*";
-                    }
-                }
-            }
-            else if(i % 3 == 1) {
-                for(int j=1; j<=a; j++) {
-                    star = star+ "*";
-                }
-            }
-            StringBuilder sb = new StringBuilder(star);
-            K[i-1] = String.valueOf(sb);
-            System.out.println(K[i-1]);
-            star = "";
         }
     }
 }
--- OneStage/Q2447_2.java
+++ OneStage/Q2447_2.java
@@ -1,4 +1,30 @@
+//https://www.acmicpc.net/problem/2447 별찍기 - 10
 package BaekJoon.OneStage;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Q2447_2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int a = Integer.parseInt(br.readLine());
+        String star[][] = new String[a + 1][a + 1];
+        for (int i = 1; i <= a; i++) {
+            for (int j = 1; j <= a; j++) {
+                if (i % 3 == 2) {
+                    if (j % 3 == 2) star[i][j] = " ";
+                    else star[i][j] = "*";
+                }
+                else star[i][j] = "*";
+
+            }
+        }
+        for(int i=1; i<=a; i++) {
+            for(int j=1; j<=a; j++) {
+
+            }
+        }
+    }
 }
