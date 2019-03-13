948db4a9b532501370805e5d9454ef4164ee4f03
etg6550 <etg9307@naver.com>
2019-03-13 09:56:21 +0900

    #1316

--- OneStage/Q1316.java
+++ OneStage/Q1316.java
@@ -1,4 +1,24 @@
 package BaekJoon.OneStage;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Q1316 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int K = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < K; i++) {
+            String Ab = br.readLine();
+
+            Character[] token = new Character[Ab.length()];
+            for (int j = 0; j < Ab.length(); j++) {
+                token[j] = Ab.charAt(j);
+            }
+            System.out.println(token);
+
+        }
+    }
 }
