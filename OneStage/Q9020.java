c5922106f2953a0e18ae1fa8b3e3c947b581b56f
etg6550 <etg9307@naver.com>
2019-03-13 10:00:11 +0900

    #9020

--- OneStage/Q9020.java
+++ OneStage/Q9020.java
@@ -1,4 +1,26 @@
 package BaekJoon.OneStage;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Q9020 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int K = Integer.parseInt(br.readLine());
+
+        int store[] = new int[K];
+        for(int i=0; i<K; i++) {
+            int J = Integer.parseInt(br.readLine());
+            store[i] = J;
+        }
+        int count = 0;
+        for(int st : store) {
+            for(int i=2; i<st; i++) {
+                if(st%i==0) count++;
+            }
+        }
+    }
 }
